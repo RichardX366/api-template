0fe17628de5265d9e8a27c6e332601823c5a2862
Richard <richardx366@gmail.com>
2023-07-22 03:23:58 -0400

    updated auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
-import { logIn, signUp } from '../controllers/auth';
+import {
+  logIn,
+  refreshToken,
+  signUp,
+  updateAuthLevel,
+  updatePassword,
+  updateUser,
+} from '../controllers/auth';
 
 const authRouter = Router();
 
 authRouter.post('/login', logIn);
 authRouter.post('/signUp', signUp);
+authRouter.post('/refresh', refreshToken);
+authRouter.put('/updatePassword/:id', updatePassword);
+authRouter.put('/authLevel/:id', updateAuthLevel);
+authRouter.put('/:id', updateUser);
 
 export default authRouter;
